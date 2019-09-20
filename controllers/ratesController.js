ba7764a2540868787cba647dd4abc9b07b80d03c
Shakhruz Ashirov <shakhruz@gmail.com>
2019-09-20 17:51:03 +0500

    автоматический расчет rates

--- controllers/ratesController.js
+++ controllers/ratesController.js
@@ -1,16 +1,38 @@
+const rates = require('../rates.js')
+const spread = 0.05
+
 exports.getAllRates = (req, res) => {
+    const btc_price = rates.btc_price() * (1/rates.bip_price())
+
+    console.log("btc price: ", btc_price)
+    const btc_buy = btc_price - btc_price * spread
+    const btc_sell = btc_price + btc_price * spread
+
+    const usdt_price  = 1/ rates.bip_price()
+    console.log("usdt_price: ", usdt_price)
+
+    const usdt_buy = usdt_price - usdt_price * spread
+    const usdt_sell = usdt_price + usdt_price * spread
+
     const allRates = {
         rates: [
-            {coin: "BTC", buy: 104624, sell: 115637, reserve: 10},
-            {coin: "USDT", buy: 10.44, sell: 11.54, reserve: 10000}
+            {coin: "BIP", buy: 1, sell: 1, reserve: 50000},
+            {coin: "BTC", buy: btc_buy, sell: btc_sell, reserve: 10},
+            {coin: "USDT", buy: usdt_buy, sell: usdt_sell, reserve: 10000}
         ]
     }
-    res.json(allRates);
+    res.json(allRates);    
 };
 
 exports.getAllCoins = (req, res) => {
     const allCoins = {
-        coins: ["BIP", "BTC", "USDT", "ETH"]
+        coins: ["BIP", "BTC", "USDT"]
     }
     res.json(allCoins);
 };
+
+exports.calcRate = (req, res) => {
+    const result = {result: req.params.sell_amount}
+
+    res.json(result);
+}
