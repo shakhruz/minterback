831ce4fcf762efe356f89100dfdf41fceb4bc1f2
Shakhruz Ashirov <shakhruz@gmail.com>
2019-09-30 17:35:42 +0500

    sendAllToReserve - отправляю все полученные BIP в резерв

--- controllers/contractController.js
+++ controllers/contractController.js
@@ -1,212 +1,258 @@
-import contractModel from '../models/contractModel.js';
+import contractModel from "../models/contractModel.js";
 
-const minter = require('../minter.js')
-const bcoin = require('../bcoin.js')
-const rates = require('../rates.js')
-const data = require('../data.js')
+const minter = require("../minter.js");
+const bcoin = require("../bcoin.js");
+const rates = require("../rates.js");
+const data = require("../data.js");
 
 exports.getContract = (req, res) => {
-    contractModel.findById(req.params.contractId, (err, contract) => {
-        if (err) {
-            res.send(err);
-        }
+  contractModel.findById(req.params.contractId, (err, contract) => {
+    if (err) {
+      res.send(err);
+    }
 
-        contract.receivingPrivKey = ''
-        res.json(contract);
-    });
+    contract.receivingPrivKey = "";
+    res.json(contract);
+  });
 };
 
 exports.getAllContracts = (req, res) => {
-    contractModel.find({}, (err, contracts) => {
-        if (err) {
-            res.send(err);
-        }
+  contractModel.find({}, (err, contracts) => {
+    if (err) {
+      res.send(err);
+    }
 
-        res.json(contracts);
-    });
+    res.json(contracts);
+  });
 };
 
 exports.createContract = (req, res) => {
-    const newContract = new contractModel(req.body);
-    console.log("new contract: ", newContract)
-    newContract.buy_amount = 0;
+  const newContract = new contractModel(req.body);
+  console.log("new contract: ", newContract);
+  newContract.buy_amount = 0;
 
-    if (newContract.sell_coin == "BIP") {
-        console.log("generate BIP address to receive coins")
-        const wallet = minter.generateWallet()
-        console.log("wallet: ", wallet)
-        newContract.receivingAddress = wallet.address
-        newContract.receivingPrivKey = wallet.priv_key
-        newContract.state = "waiting for payment"
+  if (newContract.sell_coin == "BIP") {
+    console.log("generate BIP address to receive coins");
+    const wallet = minter.generateWallet();
+    console.log("wallet: ", wallet);
+    newContract.receivingAddress = wallet.address;
+    newContract.receivingPrivKey = wallet.priv_key;
+    const priv_key = newContract.receivingPrivKey;
+    newContract.state = "waiting for payment";
+
+    newContract.save((err, contract) => {
+      if (err) {
+        res.send(err);
+      }
+
+      console.log("returning new contract: ", contract);
+      newContract.receivingPrivKey = null; // прячем ключ
+      res.json(newContract); // возвращаем без ключа
+
+      // восстанавливаем ключ и продолжаем
+      contract.receivingPrivKey = priv_key;
+      processContract(contract);
+    });
+  } else {
+    if (newContract.sell_coin == "BTC") {
+      console.log("generating new BTC address to receive payment");
+      bcoin.generateReserveAddress(input_address => {
+        console.log("new address: ", input_address);
+        newContract.receivingAddress = input_address;
+        newContract.receivingPrivKey = "";
+        newContract.state = "waiting for payment";
 
         newContract.save((err, contract) => {
-            if (err) {
-                res.send(err);
-            }
-    
-            console.log("returning new contract: ", contract)
-    
-            contract.receivingPrivKey = null // прячем ключ
-            newContract.receivingPrivKey = ''
-            res.json(contract);
-            processContract(contract)
-        });
-    } else {
-        if (newContract.sell_coin == "BTC") {
-            console.log("generating new BTC address to receive payment");
-            bcoin.generateReserveAddress((input_address)=>{
-                console.log("new address: ", input_address);
-                newContract.receivingAddress = input_address
-                newContract.receivingPrivKey = ''
-                newContract.state = "waiting for payment"
+          if (err) {
+            res.send(err);
+          }
 
-                newContract.save((err, contract) => {
-                    if (err) {
-                        res.send(err);
-                    }
-            
-                    console.log("returning new contract: ", contract)
-            
-                    contract.receivingPrivKey = null // прячем ключ
-                    newContract.receivingPrivKey = ''
-                    res.json(contract);
-                    processContract(contract)
-                });                
-            })
-        }
+          console.log("returning new contract: ", contract);
+          res.json(contract);
+          processContract(contract);
+        });
+      });
     }
+  }
 };
 
 function processContract(contract) {
-    if (contract.sell_coin == "BIP") {
-        minter.waitForBIPPayment(contract.receivingAddress, (trx) => {
-            console.log("got BIP payment: ", trx)
-            contract.receivedCoins = trx.data.value * 100
-            contract.state = "payment received"
-            contract.fromAddress = trx.from
-            contract.incomingTx = trx.hash
-            saveContract(contract)
-            completeContract(contract)    
-        })    
-    } else {
-        if (contract.sell_coin == "BTC") {
-            console.log("now wait for BTC payment to address ", contract.receivingAddress)
-            bcoin.waitForPayment(data.BTCReserveAccountName, contract.receivingAddress, (value, details)=>{
-                console.log("got BTC payment: ", details, "value: ", value)
-                contract.state = "payment received"
-                contract.receivedCoins = value
-                contract.fromAddress = details.inputs[0].address
-                contract.incomingTx = details.hash
-                saveContract(contract)
-                completeContract(contract)        
-            })
-        } else {
-            console.log("принимаем только BIP, ", contract.sell_coin, " не умеем")
+  console.log("process contract: ", contract);
+  if (contract.sell_coin == "BIP") {
+    minter.waitForBIPPayment(contract.receivingAddress, trx => {
+      console.log("got BIP payment: ", trx);
+      contract.receivedCoins = trx.data.value;
+      contract.state = "payment received";
+      contract.fromAddress = trx.from;
+      contract.incomingTx = trx.hash;
+      saveContract(contract);
+      sendAllBIPToReserve(contract);
+      completeContract(contract);
+    });
+  } else {
+    if (contract.sell_coin == "BTC") {
+      console.log(
+        "now wait for BTC payment to address ",
+        contract.receivingAddress
+      );
+      bcoin.waitForPayment(
+        data.BTCReserveAccountName,
+        contract.receivingAddress,
+        (value, details) => {
+          console.log("got BTC payment: ", details, "value: ", value);
+          contract.state = "payment received";
+          contract.receivedCoins = value;
+          contract.fromAddress = details.inputs[0].address;
+          contract.incomingTx = details.hash;
+          saveContract(contract);
+          completeContract(contract);
         }
+      );
+    } else {
+      console.log("принимаем только BIP, ", contract.sell_coin, " не умеем");
     }
+  }
 }
 
 function saveContract(contract) {
-    console.log('save contract: ', contract)
-    contractModel.updateOne({_id: contract._id}, contract,
-        (err, contract) => {
-            if (err) {
-                console.log(err);
-            }
+  console.log("save contract: ", contract);
+  contractModel.updateOne({ _id: contract._id }, contract, (err, contract) => {
+    if (err) {
+      console.log(err);
+    }
 
-            console.log("completed updating contract: ", contract);
-        }
-    );
+    console.log("completed updating contract: ", contract);
+  });
 }
 
 // Исполнить контракт
 function completeContract(contract) {
-    console.log("complete contract...")
-    if (contract.buy_coin == "BTC") {
-        // отправляем BTC
-        rates.getRates((btc_price, bip_price) => {
-            btc_price = btc_price * (1/bip_price)
+  console.log("complete contract...");
+  if (contract.buy_coin == "BTC") {
+    // отправляем BTC
+    rates.getRates((btc_price, bip_price) => {
+      btc_price = btc_price * (1 / bip_price);
 
-            console.log("btc price: ", btc_price)
-            const btc_sell = btc_price + btc_price * rates.spread
-            console.log("btc buy: ", btc_sell)
-            
-            contract.send_amount = Math.trunc(contract.receivedCoins / btc_sell * 100000000)
-            
-            console.log(`надо отправить ${contract.send_amount}sat на адрес ${contract.toAddress}`)
-            contract.state = "sending"
-            saveContract(contract)
-            bcoin.sendFromReserve(contract.send_amount, contract.toAddress, (result, arg) => {
-                if (result) {
-                    console.log("successfuly sent bcoin: ", arg.hash)
-                    contract.state = "completed"
-                    contract.outgoingTx = arg.hash
-                    contract.fee_sat = arg.fee
-                } else {
-                    console.log("error sending bcoin: ", arg)
-                    contract.state = "error"
-                    contract.message = arg
-                }
-                saveContract(contract)
-            })
-        })
-    } else {
-        if (contract.buy_coin == "BIP") {
-            console.log("sending BIP")
-            rates.getRates((btc_price, bip_price) => {
-                btc_price = btc_price * (1/bip_price)
+      console.log("btc price: ", btc_price);
+      const btc_sell = btc_price + btc_price * rates.spread;
+      console.log("btc buy: ", btc_sell);
 
-                console.log("btc price: ", btc_price)
-                const btc_buy = btc_price - btc_price * rates.spread
-                console.log("btc buy: ", btc_buy)
-                
-                contract.send_amount = Math.trunc(contract.receivedCoins * btc_buy / 100000000)
-                console.log("send_amount: ", contract.send_amount)
-                console.log(`надо отправить ${contract.send_amount} BIP на адрес ${contract.toAddress}`)
-                contract.state = "sending"
-                saveContract(contract)
-                minter.sendFromReserve(contract.send_amount, contract.toAddress, (result, arg) => {
-                    if (result) {
-                        console.log("successfuly sent BIP: ", arg)
-                        contract.state = "completed"
-                        contract.outgoingTx = arg
-                        contract.fee_sat = 0.01
-                    } else {
-                        console.log("error sending BIP: ", arg)
-                        contract.state = "error"
-                        contract.message = arg
-                    }
-                    saveContract(contract)
-                })
-            })    
+      contract.send_amount = Math.trunc(
+        (contract.receivedCoins / btc_sell) * 100000000
+      );
+
+      console.log(
+        `надо отправить ${contract.send_amount}sat на адрес ${contract.toAddress}`
+      );
+      contract.state = "sending";
+      saveContract(contract);
+      bcoin.sendFromReserve(
+        contract.send_amount,
+        contract.toAddress,
+        (result, arg) => {
+          if (result) {
+            console.log("successfuly sent bcoin: ", arg.hash);
+            contract.state = "completed";
+            contract.outgoingTx = arg.hash;
+            contract.fee_sat = arg.fee;
+          } else {
+            console.log("error sending bcoin: ", arg);
+            contract.state = "error";
+            contract.message = arg;
+          }
+          saveContract(contract);
         }
-        console.log("не могу исполнить контракт, непонятно что покупать: ", contract.buy_coin)
+      );
+    });
+  } else {
+    if (contract.buy_coin == "BIP") {
+      console.log("sending BIP");
+      rates.getRates((btc_price, bip_price) => {
+        btc_price = btc_price * (1 / bip_price);
+
+        console.log("btc price: ", btc_price);
+        const btc_buy = btc_price - btc_price * rates.spread;
+        console.log("btc buy: ", btc_buy);
+
+        contract.send_amount = Math.trunc(
+          (contract.receivedCoins * btc_buy) / 100000000
+        );
+        console.log("send_amount: ", contract.send_amount);
+        console.log(
+          `надо отправить ${contract.send_amount} BIP на адрес ${contract.toAddress}`
+        );
+        contract.state = "sending";
+        saveContract(contract);
+        minter.sendFromReserve(
+          contract.send_amount,
+          contract.toAddress,
+          (result, arg) => {
+            if (result) {
+              console.log("successfuly sent BIP: ", arg);
+              contract.state = "completed";
+              contract.outgoingTx = arg;
+              contract.fee_sat = 0.01;
+            } else {
+              console.log("error sending BIP: ", arg);
+              contract.state = "error";
+              contract.message = arg;
+            }
+            saveContract(contract);
+          }
+        );
+      });
     }
+    console.log(
+      "не могу исполнить контракт, непонятно что покупать: ",
+      contract.buy_coin
+    );
+  }
+}
+
+function sendAllBIPToReserve(contract) {
+  console.log(
+    "sending all BIP to reserve account from ",
+    contract.receivingAddress,
+    contract.receivingPrivKey
+  );
+  minter.sendAllToReserve(
+    contract.receivingAddress,
+    contract.receivingPrivKey,
+    result => {
+      console.log("sent all to reserve: ", result);
+    }
+  );
 }
 
 exports.updateContract = (req, res) => {
-    contractModel.findOneAndUpdate({
-        _id: req.params.contractId
-    }, req.body,
-        (err, contract) => {
-            if (err) {
-                res.send(err);
-            }
+  contractModel.findOneAndUpdate(
+    {
+      _id: req.params.contractId
+    },
+    req.body,
+    (err, contract) => {
+      if (err) {
+        res.send(err);
+      }
 
-            res.json(contract);
-        });
+      res.json(contract);
+    }
+  );
 };
 
 exports.deleteContract = (req, res) => {
-    contractModel.remove({
-        _id: req.params.contractId
-    }, (err) => {
-        if (err) {
-            res.send(err);
-        }
+  contractModel.remove(
+    {
+      _id: req.params.contractId
+    },
+    err => {
+      if (err) {
+        res.send(err);
+      }
 
-        res.json({
-            message: `contract ${req.params.contractId} successfully deleted`
-        });
-    });
+      res.json({
+        message: `contract ${req.params.contractId} successfully deleted`
+      });
+    }
+  );
 };
--- minter.js
+++ minter.js
@@ -1,101 +1,122 @@
 // const minterWallet = require('./minterhd.js')
-const fetch = require('node-fetch') 
-const minterWallet = require('minterjs-wallet') 
-const minterLib = require('minter-js-sdk')
+const fetch = require("node-fetch");
+const minterWallet = require("minterjs-wallet");
+const minterLib = require("minter-js-sdk");
 
-const bip_api_url = 'https://explorer-api.apps.minter.network/api/'
-const data = require('./data.js')
+const bip_api_url = "https://explorer-api.apps.minter.network/api/";
+const data = require("./data.js");
 
-import {walletFromMnemonic} from 'minterjs-wallet';
+import { walletFromMnemonic } from "minterjs-wallet";
 const wallet_reserve = walletFromMnemonic(data.BIPReserveMnemonic);
 
-exports.generateWallet = function () {
-    const wallet = minterWallet.generateWallet()
-    const address = wallet.getAddressString()
-    const priv_key = wallet.getPrivateKeyString()
-    return {address: address, priv_key: priv_key}
-}
+exports.generateWallet = function() {
+  const wallet = minterWallet.generateWallet();
+  const address = wallet.getAddressString();
+  const priv_key = wallet.getPrivateKeyString();
+  return { address: address, priv_key: priv_key };
+};
 
-exports.waitForBIPPayment = function (address, callback) {
-    console.log("waiting for a payment on address " + address)
-    let tries = 60 * 60
-    let interval = setInterval(()=> {
-      console.log("checking trxs on address " + address)
-      fetch(`${bip_api_url}v1/addresses/${address}/transactions`)
+exports.waitForBIPPayment = function(address, callback) {
+  console.log("waiting for a payment on address " + address);
+  let tries = 60 * 60;
+  let interval = setInterval(() => {
+    console.log("checking trxs on address " + address);
+    fetch(`${bip_api_url}v1/addresses/${address}/transactions`)
       .then(res => res.json())
       .then(json => {
-          const data = json.data
-          if (data.length >0 ) {
-              console.log("есть транзакции: ", data.length)
-              for(let trx of data) {
-                if (trx.data.coin == "BIP" && trx.data.to == address) {
-                  console.log(`совпало: ${trx.data.value} ${trx.data.coin} fee: ${trx.fee} from: ${trx.from}  to ${trx.data.to}`)
-                  clearInterval(interval)
-                  callback(trx)
-                }
-              }
+        const data = json.data;
+        if (data.length > 0) {
+          console.log("есть транзакции: ", data.length);
+          for (let trx of data) {
+            if (trx.data.coin == "BIP" && trx.data.to == address) {
+              console.log(
+                `совпало: ${trx.data.value} ${trx.data.coin} fee: ${trx.fee} from: ${trx.from}  to ${trx.data.to}`
+              );
+              clearInterval(interval);
+              callback(trx);
+            }
           }
-      })
-  
-      tries -= 1
-      if (tries < 1) clearInterval(interval)
-    }, 1000)    
-}
+        }
+      });
 
-exports.getBalances = function (address, callback) {
-    console.log("get balance for address " + address)  
-    fetch(`${bip_api_url}v1/addresses/${address}`)
+    tries -= 1;
+    if (tries < 1) clearInterval(interval);
+  }, 1000);
+};
+
+exports.getBalances = function(address, callback) {
+  console.log("get balance for address " + address);
+  fetch(`${bip_api_url}v1/addresses/${address}`)
     .then(res => res.json())
     .then(json => {
-        if (json.data && json.data.balances) {
-            callback(json.data.balances)    
-        }
-    })    
-}
+      if (json.data && json.data.balances) {
+        callback(json.data.balances);
+      }
+    });
+};
 
 exports.getBIPBalance = function(address, callback) {
-    console.log("get BIP balance for " + address)
-    this.getBalances(address, (balances) => {
-        let bipBalance = 0
-        if (balances.length > 0) {
-            for(let b of balances) {
-                if (b.coin == "BIP") bipBalance = Number(b.amount)
-            }
-            callback(bipBalance)    
-        }
-    })
-}
+  console.log("get BIP balance for " + address);
+  this.getBalances(address, balances => {
+    let bipBalance = 0;
+    if (balances.length > 0) {
+      for (let b of balances) {
+        if (b.coin == "BIP") bipBalance = Number(b.amount);
+      }
+      callback(bipBalance);
+    }
+  });
+};
 
-exports.sendFromReserve = function (amount, address, callback) {
-    console.log("send ", amount, " to ", address)
-    sendBIP(wallet_reserve.getPrivateKeyString(), address, amount, callback)
-}
+exports.sendFromReserve = function(amount, address, callback) {
+  console.log("send ", amount, " to ", address);
+  sendBIP(wallet_reserve.getPrivateKeyString(), address, amount, callback);
+};
+
+exports.sendAllToReserve = function(from, priv_key, callback) {
+  this.getBIPBalance(from, balance_bip => {
+    if (balance_bip > 0.01) {
+      balance_bip -= 0.01;
+      const to = wallet_reserve.getAddressString();
+      console.log("send ", balance_bip, " to ", to);
+      sendBIP(priv_key, to, balance_bip, result => {
+        callback(result);
+      });
+    }
+  });
+};
 
 function sendBIP(privateKey, to, value, callback) {
-    let amount = Number(value)
-    console.log("send " + amount + "BIP " +" to " + to)
-    // const minterSDK = new minterLib.Minter({chainId: 1,apiType: 'node', baseURL: 'https://minter-node-1.testnet.minter.network'});
-    // const minterSDK = new minterLib.Minter({chainId: 1, apiType: 'node', baseURL: 'https://minter-node-1.testnet.minter.network'});
-    // const minterSDK = new minterLib.Minter({chainId: 1, apiType: 'gate', baseURL: 'https://gate.minter.network/api/v1/'});
-    const minterSDK = new minterLib.Minter({chainId: 1, apiType: 'node', baseURL: 'https://api.minter.stakeholder.space'});
-    const txParams = new minterLib.SendTxParams({
-        privateKey: privateKey,
-        chainId: 1,
-        address: to,
-        amount: amount,
-        coinSymbol: 'BIP',
-        feeCoinSymbol: 'BIP',
-        gasPrice: 1
+  let amount = Number(value);
+  console.log("send " + amount + "BIP " + " to " + to);
+  // const minterSDK = new minterLib.Minter({chainId: 1,apiType: 'node', baseURL: 'https://minter-node-1.testnet.minter.network'});
+  // const minterSDK = new minterLib.Minter({chainId: 1, apiType: 'node', baseURL: 'https://minter-node-1.testnet.minter.network'});
+  // const minterSDK = new minterLib.Minter({chainId: 1, apiType: 'gate', baseURL: 'https://gate.minter.network/api/v1/'});
+  const minterSDK = new minterLib.Minter({
+    chainId: 1,
+    apiType: "node",
+    baseURL: "https://api.minter.stakeholder.space"
+  });
+  const txParams = new minterLib.SendTxParams({
+    privateKey: privateKey,
+    chainId: 1,
+    address: to,
+    amount: amount,
+    coinSymbol: "BIP",
+    feeCoinSymbol: "BIP",
+    gasPrice: 1
+  });
+
+  minterSDK
+    .postTx(txParams)
+    .then(txHash => {
+      callback(true, txHash);
     })
-    
-    minterSDK.postTx(txParams)
-        .then((txHash) => {
-            callback(true, txHash)
-        }).catch((error) => {
-            const errMessage = error.response.data.error;
-            console.log(errMessage)
-            callback(false, errMessage)
-        })
+    .catch(error => {
+      const errMessage = error.response.data.error;
+      console.log(errMessage);
+      callback(false, errMessage);
+    });
 }
 
 // HD
@@ -114,4 +135,3 @@ function sendBIP(privateKey, to, value, callback) {
 // import {walletFromExtendedPrivateKey} from 'minterjs-wallet';
 // const wallet = walletFromExtendedPrivateKey('xprv9s21ZrQH143K4KqQx9Zrf1eN8EaPQVFxM2Ast8mdHn7GKiDWzNEyNdduJhWXToy8MpkGcKjxeFWd8oBSvsz4PCYamxR7TX49pSpp3bmHVAY');
 // const key = minterWallet.hdKeyFromSeed()
-
