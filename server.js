b6a3f74f7689d1b82410060d26fd93de26280320
Shakhruz Ashirov <shakhruz@gmail.com>
2019-10-18 07:05:03 +0500

    добавил ws сервер

--- server.js
+++ server.js
@@ -1,3 +1,48 @@
-import app from './app';
-const port = process.env.PORT || '3333'; app.listen(port); 
+import app from "./app";
+const port = process.env.PORT || "3333";
+app.listen(port);
 console.log(`Listening on port ${port}`);
+
+const WebSocketServer = require("ws").Server;
+//const wss = new WebSocketServer({ server: server });
+const wss = new WebSocketServer({ port: 9090 });
+
+wss.on("connection", ws => {
+  ws.isAlive = true;
+  ws.on("pong", () => {
+    ws.isAlive = true;
+  });
+  ws.ping(_.noop);
+  ws.on("error", e => {
+    console.error(new Date(), "[WS] connection error:", e);
+  });
+});
+
+setInterval(() => {
+  wss.clients.forEach(ws => {
+    if (!ws.isAlive) {
+      console.log(new Date(), "[WS] stale websocket client, terminiating..");
+      return ws.terminate();
+    }
+
+    ws.isAlive = false;
+    ws.ping(_.noop);
+  });
+}, 10 * 1000);
+
+// broadcast function
+const broadcast = data => {
+  if (_.isEmpty(data)) {
+    return;
+  }
+
+  const payload = JSON.stringify(data);
+
+  wss.clients.forEach(ws => {
+    ws.send(payload, err => {
+      if (err) {
+        console.log(new Date(), "[WS] unable to send data to client:", err);
+      }
+    });
+  });
+};
